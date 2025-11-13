e136f645ce2a102943a3ebd734326f8dd7d04dce
haeseon lee <haeseon4816@gmail.com>
2025-11-13 16:28:29 +0900

    feat: 스페셜 날짜에 방문시 할인 적용 메서드 구현

--- src/SpecialEvent.js
+++ src/SpecialEvent.js
@@ -1,5 +1,11 @@
 export const SpecialEvent = {
   applySpecialDiscount(visitDate) {
     const specialDay = [3, 10, 17, 24, 25, 31];
+
+    const visitDay = visitDate.getVisitDate();
+    if (specialDay.includes(visitDay)) {
+      return 1000;
+    }
+    return 0;
   },
 };
