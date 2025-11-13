1ae143b08795514ef7d334b663b57fbbf661fadb
haeseon lee <haeseon4816@gmail.com>
2025-11-13 13:29:58 +0900

    feat: 크리스마스 디데이 할인 기능 메서드 구현

--- src/ChristmasDDay.js
+++ src/ChristmasDDay.js
@@ -1,14 +1,23 @@
 export const ChristmasDDay = {
-  isDateWithinRange(targetDate) {
+  isDateWithinRange(visitDate) {
     const startDate = new Date("2023-12-1");
     const endDate = new Date("2023-12-25");
 
     if (
-      targetDate.getTime() >= startDate.getTime() &&
-      targetDate.getTime() <= endDate.getTime()
+      visitDate.getTime() >= startDate.getTime() &&
+      visitDate.getTime() <= endDate.getTime()
     ) {
       return true;
     }
     return false;
+  },
+
+  applyDDayDiscount(visitDate) {
+    if (!this.isDateWithinRange(visitDate)) {
+      return 0;
+    }
+
+    const visitDay = visitDate.getDate();
+    return 1000 + (visitDay - 1) * 100;
   },
 };
