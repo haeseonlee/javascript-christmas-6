cefcfd7496e4ece15a9d751ff4f8e3dbea5c04e6
haeseon lee <haeseon4816@gmail.com>
2025-11-14 15:02:20 +0900

    feat: 출력 기능 메서드 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,103 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
+import { Console } from "@woowacourse/mission-utils";
+import { Badge } from "./Badge.js";
+
+const OutputView = {
+  printWelcomeMessage() {
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n");
+  },
+
+  printVisitMessage(visitDay) {
+    Console.print(
+      "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
+      visitDay
+    );
+  },
+
+  printMenu(orders) {
+    Console.print("\n<주문 메뉴>");
+    for (let order of orders) {
+      Console.print("%s %d개\n", order.getName(), order.getQuantity());
     }
-    // ...
-}
+  },
+
+  printBeforeDiscount(payment) {
+    Console.print("\n<할인 전 총주문 금액>");
+    Console.print("%s원\n", payment.getBeforeAmount().toLocaleString());
+  },
+
+  printGiftEvent(payment) {
+    Console.print("\n<증정 메뉴>");
+    if (payment.getGiftAmount() === 0) {
+      Console.print("없음\n");
+      return;
+    }
+    Console.print("샴페인 1개\n");
+  },
+
+  printBenefitDetails(payment, visitDate) {
+    Console.print("\n<혜택 내역>");
+    const totalBenefit =
+      payment.getDDayAmount() +
+      payment.getWeekAmount() +
+      payment.getSpecialAmount() +
+      payment.getGiftAmount();
+    if (totalBenefit === 0) {
+      Console.print("없음\n");
+      return;
+    }
+    Console.print(
+      `크리스마스 디데이 할인 : -${payment
+        .getDDayAmount()
+        .toLocaleString()}원\n`
+    );
+    if (visitDate.getDayOfWeek() === 5 || visitDate.getDayOfWeek() === 6) {
+      Console.print(
+        `주말 할인 : -${payment.getWeekAmount().toLocaleString()}원\n`
+      );
+    } else {
+      Console.print(
+        `평일 할인 : -${payment.getWeekAmount().toLocaleString()}원\n`
+      );
+    }
+    Console.print(
+      `특별 할인: -${payment.getSpecialAmount().toLocaleString()}원\n`
+    );
+    Console.print(
+      `증정 이벤트 : -${payment.getGiftAmount().toLocaleString()}원\n`
+    );
+  },
+
+  printTotalBenefitAmount(payment) {
+    const totalBenefit =
+      payment.getDDayAmount() +
+      payment.getWeekAmount() +
+      payment.getSpecialAmount() +
+      payment.getGiftAmount();
+    Console.print("\n<총혜택 금액>");
+    Console.print(`-${totalBenefit.toLocaleString()}원\n`);
+  },
+
+  printAfterDiscountAmount(payment) {
+    const discountAmount =
+      payment.getBeforeAmount() -
+      payment.getDDayAmount() -
+      payment.getWeekAmount() -
+      payment.getSpecialAmount() -
+      payment.getGiftAmount();
+    Console.print("\n<할인 후 예상 결제 금액>");
+    Console.print(`${discountAmount.toLocaleString()}원`);
+  },
+
+  printBadge(payment) {
+    const discountAmount =
+      payment.getBeforeAmount() -
+      payment.getDDayAmount() -
+      payment.getWeekAmount() -
+      payment.getSpecialAmount() -
+      payment.getGiftAmount();
+    Console.print("\n<12월 이벤트 배지>");
+    Console.print(`${Badge.applyBadge(discountAmount)}`);
+  },
+};
+
+export default OutputView;
--- src/WeekDiscount.js
+++ src/WeekDiscount.js
@@ -1,4 +1,4 @@
-import { Menu } from "./Menu.js";
+import { findMenuByName } from "./Menu.js";
 
 export const WeekDiscount = {
   applyWeekDiscount(visitDate, orders) {
@@ -27,7 +27,7 @@ export const WeekDiscount = {
   calculateWeekDay(orders) {
     let menuCount = 0;
     for (let order of orders) {
-      const menu = Menu.findMenuByName(order.getName());
+      const menu = findMenuByName(order.getName());
       if (menu.type === "디저트") {
         const quantity = order.getQuantity();
         menuCount += quantity;
