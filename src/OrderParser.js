5ffac81b811cfd8b01888aa8564f2638e31e4bca
haeseon lee <haeseon4816@gmail.com>
2025-11-15 12:12:39 +0900

    feat: 메뉴 최대 20개까지 주문 검증 메서드 구현

--- src/OrderParser.js
+++ src/OrderParser.js
@@ -24,7 +24,12 @@ export const OrderParser = {
       const [name, quantityStr] = menuPairs;
 
       const quantity = Number(quantityStr);
-      if (isNaN(quantity) || quantity < 1 || !Number.isInteger(quantity)) {
+      if (
+        isNaN(quantity) ||
+        quantity < 1 ||
+        quantity > 20 ||
+        !Number.isInteger(quantity)
+      ) {
         throw new Error(
           "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
         );
@@ -45,8 +50,18 @@ export const OrderParser = {
     const isOrderOnlyDrinks = orders.every(
       (order) => findMenuByName(order.name).type === "음료"
     );
+
     if (isOrderOnlyDrinks) {
       throw new Error("[ERROR] 음료만 주문할 수 없습니다.");
+    }
+
+    let totalQuantity = orders.reduce(
+      (sum, order) => sum + order.getQuantity(),
+      0
+    );
+
+    if (totalQuantity > 20) {
+      throw new Error("[ERROR] 메뉴는 최대 20개까지만 주문 가능합니다.");
     }
     return orders;
   },
